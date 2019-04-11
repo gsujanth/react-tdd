05eb09355ed84c094b9665974747fc37242fc725
gsujanth <princesujanth@gmail.com>
2019-04-10 23:32:21 -0400

    this commit

--- src/PersonEdit.js
+++ src/PersonEdit.js
@@ -19,19 +19,17 @@ export default class PersonEdit extends Component{
 render(){
     return(
         <div>
-        <h2>Person Edit Page</h2>
-        <form onSubmit={() => this.props.savePerson(this.state)}>
-            <label htmlFor='firstName'>First Name:</label>
-            <input type='text' id='firstName' onChange={this.handleChange} value={this.state.firstName} name='firstName' required></input><br/><br/>
-            <label htmlFor='lastName'>Last Name:</label>
-            <input type='text' id='lastName' onChange={this.handleChange} value={this.state.lastName} name='lastName' required></input><br/><br/>
-            <button type="submit" name='saveButton'>Save</button> &nbsp; 
-            <button type="button" name='deleteButton' onClick={()=>{this.props.deletePerson(this.state)}}>Delete</button> &nbsp;
-            <button type="button" name='cancelButton' onClick={()=>{this.props.cancel()}}>Cancel</button>
-        </form>
-        
+            <h2>Person Edit Page</h2>
+            <form onSubmit={() => this.props.savePerson(this.state)}>
+                <label htmlFor='firstName'>First Name:</label>
+                <input type='text' id='firstName' onChange={this.handleChange} value={this.state.firstName} name='firstName' required></input><br/><br/>
+                <label htmlFor='lastName'>Last Name:</label>
+                <input type='text' id='lastName' onChange={this.handleChange} value={this.state.lastName} name='lastName' required></input><br/><br/>
+                <button type="submit" name='saveButton'>Save</button> &nbsp; 
+                <button type="button" name='deleteButton' onClick={()=>{this.props.deletePerson(this.state)}}>Delete</button> &nbsp;
+                <button type="button" name='cancelButton' onClick={()=>{this.props.cancel()}}>Cancel</button>
+            </form>
         </div>
     );
 }
-
 }
